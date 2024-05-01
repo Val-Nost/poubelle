1b0d74ae4705eb04235d1ca7bde5653e6de2c36f
AdrienCarles <adrien.carles@limayrac.fr>
2024-05-01 11:23:23 +0200

    Formulaire de modification des utilisateurs et supression AC

--- src/main/java/fr/limayrac/poubelle/controller/UtilisateurController.java
+++ src/main/java/fr/limayrac/poubelle/controller/UtilisateurController.java
@@ -6,6 +6,7 @@ import fr.limayrac.poubelle.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -34,6 +35,32 @@ public class UtilisateurController {
         String encodedPassword = passwordEncoder.encode(utilisateur.getPassword());
         utilisateur.setPassword(encodedPassword);
         utilisateurDao.save(utilisateur);
+        return "redirect:/accueil";
+    }
+    @GetMapping("/modifier/{id}")
+    public String modifierUtilisateurForm(@PathVariable Long id, Model model) {
+        Utilisateur utilisateur = utilisateurDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("utilisateur", utilisateur);
+        model.addAttribute("roles", Role.values());
+        return "modifierUtilisateur";
+    }
+    @PostMapping("/update/{id}")
+    public String modifierUtilisateur(@PathVariable Long id, @ModelAttribute Utilisateur utilisateurForm) {
+        Utilisateur utilisateur = utilisateurDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        utilisateur.setNom(utilisateurForm.getNom());
+        utilisateur.setPrenom(utilisateurForm.getPrenom());
+        utilisateur.setLogin(utilisateurForm.getLogin());
+        utilisateur.setPassword(passwordEncoder.encode(utilisateurForm.getPassword()));
+        utilisateur.setRole(utilisateurForm.getRole());
+        utilisateur.setActif(utilisateurForm.getActif());
+        utilisateurDao.save(utilisateur);
+        return "redirect:/accueil";
+    }
+    @PostMapping("/supprimer/{id}")
+    public String supprimerUtilisateur(@PathVariable Long id) {
+        utilisateurDao.deleteById(id);
         return "redirect:/accueil";
     }
 }
