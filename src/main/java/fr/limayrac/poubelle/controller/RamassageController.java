4181d6b4eec3681101e2fe95de1c3ce056022222
Val-Nost <valentin.smn81@gmail.com>
2024-06-03 15:05:17 +0200

    Update_Controller
    Ajout de différents Controller

---
+++ src/main/java/fr/limayrac/poubelle/controller/RamassageController.java
@@ -0,0 +1,103 @@
+package fr.limayrac.poubelle.controller;
+
+import fr.limayrac.poubelle.model.*;
+import fr.limayrac.poubelle.model.ramassage.Ramassage;
+import fr.limayrac.poubelle.model.ramassage.RamassageArret;
+import fr.limayrac.poubelle.model.ramassage.RamassageCyclisteVelo;
+import fr.limayrac.poubelle.service.IRamassageCyclisteVeloService;
+import fr.limayrac.poubelle.service.IRamassageService;
+import fr.limayrac.poubelle.service.IUtilisateurService;
+import fr.limayrac.poubelle.service.IVeloService;
+import fr.limayrac.poubelle.service.impl.ArretService;
+import fr.limayrac.poubelle.service.impl.RamassageArretService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/ramassage")
+public class RamassageController {
+    @Autowired
+    private IUtilisateurService utilisateurService;
+    @Autowired
+    private IVeloService veloService;
+    @Autowired
+    private IRamassageService ramassageService;
+    @Autowired
+    private IRamassageCyclisteVeloService ramassageCyclisteVeloService;
+    @Autowired
+    private ArretService arretService;
+    @Autowired
+    private RamassageArretService ramassageArretService;
+
+    @GetMapping
+    public String ramassage(Model model) {
+        model.addAttribute("cyclistes", utilisateurService.findByRole(Role.Cycliste));
+        model.addAttribute("velos", veloService.findByStatut(StatutVelo.UTILISABLE));
+        model.addAttribute("ramassagesEnCours", ramassageService.findByEnCours(true));
+        return "lancementRamassage";
+    }
+
+    @PostMapping("/choixCycliste")
+    public String choixCyclistes(Model model, @RequestParam List<Long> cyclistes) {
+        List<Utilisateur> cyclistesObj = new ArrayList<>();
+        List<Velo> velos = veloService.findByStatut(StatutVelo.UTILISABLE);
+
+        for (Long id : cyclistes) {
+            cyclistesObj.add(utilisateurService.findById(id));
+        }
+
+        Ramassage ramassage = new Ramassage();
+        ramassage.setEnCours(true);
+        ramassage = ramassageService.save(ramassage);
+
+        // On attribue chaque velos à chaque cycliste choisie
+        /*
+        Remarque on crée une liste pour éviter les appels multiples vers la BDD
+        Il est peut recommandé de faire appel à un DAO dans une boucle
+        */
+        List<RamassageCyclisteVelo> ramassageCyclisteVelos = new ArrayList<>();
+        for (int i = 0; i < cyclistesObj.size(); i++) {
+            RamassageCyclisteVelo ramassageCyclisteVelo = new RamassageCyclisteVelo();
+            ramassageCyclisteVelo.setRamassage(ramassage);
+            ramassageCyclisteVelo.setCycliste(cyclistesObj.get(i));
+            ramassageCyclisteVelo.setVelo(velos.get(i));
+            ramassageCyclisteVelos.add(ramassageCyclisteVelo);
+        }
+        ramassageCyclisteVeloService.saveAll(ramassageCyclisteVelos);
+
+        // On ajoute les arrets du ramassage
+        /*
+        Remarque on crée une liste pour éviter les appels multiples vers la BDD
+        Il est peut recommandé de faire appel à un DAO dans une boucle
+        */
+        List<RamassageArret> ramassageArrets = new ArrayList<>();
+        for (Arret arret : arretService.findAll()) {
+            RamassageArret ramassageArret = new RamassageArret();
+            ramassageArret.setArret(arret);
+            ramassageArret.setRamassage(ramassage);
+            ramassageArret.setRamasse(false);
+        }
+        ramassageArretService.saveAll(ramassageArrets);
+
+        return "redirect:/ramassage/liste";
+    }
+
+    @GetMapping("/{idRamassage}")
+    public String cyclistesRamassage(@PathVariable Long idRamassage, Model model) {
+        Ramassage ramassage = ramassageService.findById(idRamassage);
+        model.addAttribute("ramassage", ramassage);
+        return "infoRamassage";
+    }
+
+    @GetMapping("/liste")
+    public String listeRamassage(Model model) {
+        model.addAttribute("ramassagesEnCours", ramassageService.findByEnCours(true));
+        model.addAttribute("ramassagesTermine", ramassageService.findByEnCours(false));
+        return "ramassage";
+    }
+}
