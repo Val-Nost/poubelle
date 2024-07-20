cea75921b63bd672ac2dcbf581e43799602f7999
Val-Nost <valentin.smn81@gmail.com>
2024-07-20 13:38:20 +0200

    WIP_JSONArretAdjacent

--- src/main/java/fr/limayrac/poubelle/model/ArretAdjacent.java
+++ src/main/java/fr/limayrac/poubelle/model/ArretAdjacent.java
@@ -1,5 +1,7 @@
 package fr.limayrac.poubelle.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,14 +11,17 @@ import lombok.Setter;
 public class ArretAdjacent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
     @ManyToOne
     @JoinColumn(name = "arret")
+    @JsonBackReference
     private Arret arret;
     @ManyToOne
     @JoinColumn(name = "rue")
     private Rue rue;
     @ManyToOne
     @JoinColumn(name = "arretAdjacent")
+//    @JsonBackReference
     private Arret arretAdjacent;
 }
